2353bf4a4fb226ef334135900374bfee61ac9ea1
Stephen Mwangi <mwangiwahome70@gmail.com>
2024-07-11 09:08:01 +0300

    Modified the path of index image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Button from '../components/Button';
 
 const Home = () => {
   return (
-    <div id='home' className={`h-[85vh] relative w-full flex justify-center items-start bg-[url('./image333.png')] bg-cover bg-center`}>
+    <div id='home' className={`h-[85vh] relative w-full flex justify-center items-start bg-[url('./public/image333.png')] bg-cover bg-center`}>
         <div className="absolute h-[100%] w-[100%] inset-0 bg-black opacity-80"></div>
         <div className="relative flex flex-col space-y-10 justify-start items-start py-8 px-5">
           <div className='text-white flex justify-center items-center space-x-3 text-2xl max-sm:text-base'>
