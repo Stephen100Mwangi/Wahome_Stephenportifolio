3a230b6e31b21f590a8d9d79d9cf9cdec138e7a5
Stephen Mwangi <mwangiwahome70@gmail.com>
2024-11-09 21:49:36 +0300

    Added a target blank for awards

--- src/components/AwardCard.jsx
+++ src/components/AwardCard.jsx
@@ -1,23 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const AwardCard = ({institution,award,time,course,link}) => {
+const AwardCard = ({ institution, award, time, course, link }) => {
   return (
-    <a href={link} className='flex flex-col space-y-2'>
-      <div className='font-bold text-base text-orange'>{institution}</div>
-      <div className='font-normal text-base text-text'>{award} <span className='font-medium text-base text-orange mx-5'>{course}</span></div>
-      <div className='font-mono text-sm text-orange'>{time}</div>
+    <a target="_blank" href={link} className="flex flex-col space-y-2">
+      <div className="font-bold text-base text-orange">{institution}</div>
+      <div className="font-normal text-base text-text">
+        {award}{" "}
+        <span className="font-medium text-base text-orange mx-5">{course}</span>
+      </div>
+      <div className="font-mono text-sm text-orange">{time}</div>
     </a>
-  )
-}
+  );
+};
 
 AwardCard.propTypes = {
   institution: PropTypes.string,
   award: PropTypes.string,
   time: PropTypes.string,
   course: PropTypes.string,
-  link: PropTypes.string
-}
+  link: PropTypes.string,
+};
 
-export default AwardCard
+export default AwardCard;
